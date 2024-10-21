18207041755189d0816c16539db501946b543ad4
EvgenySugrob <xgenstudioz0@gmail.com>
2024-10-21 10:49:14 +0500

    AssetsImport3

---
+++ Assets/OutlineEffect/Demo/MaterialSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+using Vmaya;
+
+namespace cakeslice
+{
+    public class MaterialSwitcher : MonoBehaviour
+    {
+        public Material target;
+        public int index;
+
+        public void Update()
+        {
+#if ENABLE_INPUT_SYSTEM
+            if(VKeyboard.GetKeyDown(Key.M))
+#else
+            if (VKeyboard.GetKeyDown(KeyCode.M))
+#endif
+            {
+                Material[] materials = GetComponent<Renderer>().materials;
+                materials[index] = target;
+                GetComponent<Renderer>().materials = materials;
+            }
+        }
+    }
+}
---
+++ Assets/OutlineEffect/Demo/Toggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+using Vmaya;
+
+namespace cakeslice
+{
+    public class Toggle : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(VKeyboard.GetKeyDown(
+#if ENABLE_INPUT_SYSTEM
+                Key.K
+#else
+                KeyCode.K
+#endif
+            ))
+            {
+                GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
+            }
+        }
+    }
+}
